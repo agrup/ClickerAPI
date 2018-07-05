71b55601cab44a6f88b8138d2dc4db4b88f3cad9
woini2727 <salvador.woini@gmail.com>
2018-07-05 20:31:50 -0300

    mapa

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.principal')
 @section('hi')
-	<h1>hola</h1>
+	 <div id="map" class="mapa"></div>
+    <script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5bVgO6G2tR2LQmzAp9CIGWibfTDkqDjc&callback=initMap">
+    </script>
+  </script>
 @endsection
