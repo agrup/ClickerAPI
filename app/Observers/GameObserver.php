d467671f27245fa3a2d475855dd0406eb7b47c2d
agrup <agustinrodriguez206@gmail.com>
2018-07-25 19:27:50 -0300

    observer change

--- app/Observers/GameObserver.php
+++ app/Observers/GameObserver.php
@@ -5,10 +5,5 @@ use App\Game;
 
 class ServiceObserver
 {
-    public function creating(Game $game)
-    {
-   		//$user->api_token = bin2hex(openssl_random_pseudo_bytes(30));
-   		//$user->email = 'agustinrodriguez206@gmail.com';
-    	
-	}
+
 }
